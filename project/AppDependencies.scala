589315e16b352d9ac6716a9d1000bddadeac3af6
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-06-30 13:40:11 +0100

    CTCP-635: Add more tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,11 +1,8 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
-  private val catsVersion     = "2.7.0"
+  private val catsVersion     = "2.8.0"
   private val hmrcPlayVersion = "5.24.0"
 
   val compile = Seq(
@@ -15,12 +12,13 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % hmrcPlayVersion % "test, it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % hmrcPlayVersion,
     "org.mockito"             % "mockito-core"            % "4.5.1",
     "org.mockito"            %% "mockito-scala-scalatest" % "1.17.5",
     "org.typelevel"          %% "cats-core"               % catsVersion,
-    "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8"        % "test, it",
+    "com.vladsch.flexmark"    % "flexmark-all"            % "0.36.8",
+    "org.scalacheck"         %% "scalacheck"              % "1.15.4",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "4.0.3",
     "org.scalatestplus"      %% "mockito-3-2"             % "3.1.2.0"
-  )
+  ).map(_ % "test, it")
 }
